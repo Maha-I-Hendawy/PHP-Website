dc844116fab27990a2383e804fe218926f42a3c5
Maha-I-Hendawy <maha.i.hendawy@gmail.com>
2024-07-02 16:35:04 +0300

    added somethings

---
+++ contact.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+	<title>Contact Page</title>
+</head>
+<body>
+	<?php require 'includes/_nav.php'; ?>
+   <div class="container">
+
+  <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method='post'>
+
+  	  <div class="mb-3 mt-3">
+	    <label for="name" class="form-label">Name:</label>
+	    <input type="name" class="form-control" id="name" placeholder="Enter name" name="name">
+	  </div>
+
+	  <div class="mb-3 mt-3">
+	    <label for="email" class="form-label">Email:</label>
+	    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+	  </div>
+	  
+	 <label for="comment">Message:</label>
+
+     <textarea class="form-control" rows="5" id="comment" name="msg" style="margin-top: 10px;"></textarea>
+      <br>
+	  <button type="submit" class="btn btn-primary">Send</button>
+
+  </form>
+
+</div>
+ 
+
+     <?php require 'includes/_footer.php'; ?>
+
+
+</body>
+</html>
+
+<?php 
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+  	$name = $_POST['name'];
+
+  	$email = $_POST['email'];
+
+  	$msg = $_POST['msg'];
+  }
+
+
+?>
---
+++ includes/_nav.php
@@ -0,0 +1,33 @@
+ <nav class="navbar navbar-dark bg-dark" aria-label="First navbar example">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="../website/home.php">Website</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample01">
+        <ul class="navbar-nav me-auto mb-2">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="../website/home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../website/about.php">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../website/contact.php">Contact Us</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Service 1</a></li>
+              <li><a class="dropdown-item" href="#">Service 2</a></li>
+              <li><a class="dropdown-item" href="#">Service 3</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form role="search">
+          <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+        </form>
+      </div>
+    </div>
+  </nav>
